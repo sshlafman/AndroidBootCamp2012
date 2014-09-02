540d0a9291a84471f07d30d14ef6841940a30909
Shymon Shlafman <sshlasfman@ptc.com>
2014-09-02 09:36:41 +0300

    Lesson 12 - completed.

--- Yamba/src/com/sshlafman/yamba/UpdaterService.java
+++ Yamba/src/com/sshlafman/yamba/UpdaterService.java
@@ -1,5 +1,10 @@
 package com.sshlafman.yamba;
 
+import java.util.List;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.Twitter.Status;
+import winterwell.jtwitter.TwitterException;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -7,15 +12,34 @@ import android.util.Log;
 
 public class UpdaterService extends Service {
 	static final String TAG = "Updater Service";
+	Twitter twitter;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+		
 		Log.d(TAG, "onCreated");
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+		new Thread() {
+			public void run() {
+				try {
+					List<Status> timeline = twitter.getPublicTimeline();
+
+					for (Status status : timeline) {
+						Log.d(TAG, String.format("%s: %s", status.user.name,
+								status.text));
+					}
+				} catch (TwitterException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		
 		Log.d(TAG, "onStarted");
 		return super.onStartCommand(intent, flags, startId);
 	}
