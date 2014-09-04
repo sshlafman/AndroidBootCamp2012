6a9bf676f7007265f15031dc49e479402c3b2e3e
Shymon Shlafman <sshlasfman@ptc.com>
2014-09-04 17:41:51 +0300

    Lesson 17 completed.

---
+++ Yamba/src/com/sshlafman/yamba/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.sshlafman.yamba;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+	static final String TAG = "BootReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		context.startService( new Intent(context, UpdaterService.class) );
+		Log.d(TAG, "onReceive");
+	}
+}
